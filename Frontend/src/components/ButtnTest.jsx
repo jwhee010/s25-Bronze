752cdc2b94c9ebdd7aae839cad3c064ef920e7c5
Asia Gillispie <agill035@odu.edu>
2025-02-10 16:42:36 -0500

    Added Components Folder, 2 Images in Assets, PlaceHolders for LogIn page

---
+++ Frontend/src/components/ButtnTest.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import './ButtnTest.css'
+
+const ButtnTest = () => {
+    return(
+        <button onClick = {() => alert("This is a Placeholder")} >
+            Test Button to Database
+        </button>
+
+    );
+    
+
+};
+
+export default ButtnTest;
---
+++ Frontend/src/components/LogIn.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './LogIn.css';
+
+const LogIn = () => {
+    return(
+        <div className = 'wrapper'>
+            <p>This is for LogIn</p>
+        
+        
+        
+        
+        </div>
+
+    );
+    
+
+};
+
+export default LogIn;
