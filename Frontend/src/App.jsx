bd0761bed5bfbe1fa8d6982d6e2c2dc86a08dad8
Asia Gillispie <agill035@odu.edu>
2025-02-17 06:34:47 -0500

    Ran npm i react-router-dom --save, Added header to LogIn, Added LogInHeader.jsx and css, Removed ButtnTest

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import LogIn from './components/LogIn'
-import ButtnTest from './components/ButtnTest'
 import Footer from './components/Footer'
+import LogInHeader from './components/LogInHeader'
 
 
 function App() {
 
   return (
     <div> 
-    
+    <LogInHeader/>
     <LogIn/>
     <Footer/>
-
-    
-    
     </div>
    
    
---
+++ Frontend/src/components/LogInHeader.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './LogInHeader.css'
+function LogInHeader(){
+return(
+
+<h1 class = "header">  
+   <pre>
+   Welcome to<br/>
+   <span class = "styleName">
+    🌾LivelyShelfs🌾
+    </span>
+   </pre>
+</h1>
+
+)
+}
+export default LogInHeader
