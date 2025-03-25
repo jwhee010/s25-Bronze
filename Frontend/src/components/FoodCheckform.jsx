9b5f10debb1b4c0c10fa7f55ede916aa5135e1f8
Roman Pena <rpena003@odu.edu>
2025-03-24 20:05:45 -0400

    Added functionality to display inventory items in the dropdown for FoodCheckform

--- Frontend/src/components/FoodCheckform.jsx
+++ Frontend/src/components/FoodCheckform.jsx
@@ -1,91 +1,126 @@
-import React, { useState } from "react";
-import "./FoodCheckform.css"
+import React, { useState, useEffect } from "react";
+import "./FoodCheckform.css";
+import axios from "axios";
 
-export default function InventoryForm(props){
-    const [active, setAct] = useState(true);
+export default function InventoryForm(props) {
+  const [active, setAct] = useState(true);
 
-    // reference used for popup button https://codesandbox.io/p/sandbox/react-usestate-open-and-close-buttons-ryl84?file=%2Fsrc%2FApp.js
-    function closeItem() {
-        setAct(true);
-        //enables foodCheckform button
-        formButton.disabled = false;
-      }
+  const [foodItems, setFoodItems] = useState([]);
 
-    function openItem()  {
-        setAct(false);
-      }
+  const getFoodItems = async (token) => {
+    try{
+      const response = await axios.get('http://localhost:80/calendar', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
 
-   
-     var formButton = document.getElementById("formButton");
-    //-------For DropDown Menu-------
-    //Referenced: https://stackoverflow.com/questions/67895964/how-to-dynamically-add-more-elements-to-a-dropdown-list-with-vanilla-js 
+      setFoodItems(response.data.foodItems);
 
-    // Fill this array with food Items for the Drop Down
-    let foodItemArr = ["Shrimp", "Fish", "Crab", "Crayfish", "Fish Sticks", "Cabbage"];
-       
-     //Listen for dropdown click
-    let dropDown = document.getElementById("foodItem");
-    dropDown?.addEventListener('click', addItemsDropDown, {once:true});
-      
-    //Refreshs dropDown children, Puts values from foodItemArr[] to options in dropdown list
-     function addItemsDropDown(event){
-        //Prevents foodCheckform button from being clickable
-           formButton.disabled = true;
+    } catch (error) {
+      console.error('Error retrieving food items: ', error);
+    }
+  };
 
-           //Refreshs dropDown's children, removes all options
-           dropDown.replaceChildren();
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
 
-             event.stopPropagation();
-             event.preventDefault();
-         
-        //Takes values from foodItemArr, turn into dropDown options
-        foodItemArr.forEach(item =>{
-            var option = document.createElement("option");
-            option.text = item;
-            dropDown.appendChild(option);
-        });
+    try {
+      getFoodItems(token);
+    } catch(error) {
+      console.error('Token decoding error');
+    }
 
-      }
-      //-------------
+  }, []);
 
-      
+  // reference used for popup button https://codesandbox.io/p/sandbox/react-usestate-open-and-close-buttons-ryl84?file=%2Fsrc%2FApp.js
+  function closeItem() {
+    setAct(true);
+    //enables foodCheckform button
+    formButton.disabled = false;
+  }
 
-      
-    
+  function openItem() {
+    setAct(false);
+  }
 
-        
-      
+  var formButton = document.getElementById("formButton");
+  //-------For DropDown Menu-------
+  //Referenced: https://stackoverflow.com/questions/67895964/how-to-dynamically-add-more-elements-to-a-dropdown-list-with-vanilla-js
 
-return( 
-    
-    <div className='checkbutton'>
-         <button className = "invCheck" id="formButton" type="button" onClick={openItem}>Consumed/Spoiled Form</button> 
-        
-        <h1 className={active ? "cclose" : "copen"}>
+  // Fill this array with food Items for the Drop Down
+  // let foodItemArr = [
+  //   "Shrimp",
+  //   "Fish",
+  //   "Crab",
+  //   "Crayfish",
+  //   "Fish Sticks",
+  //   "Cabbage",
+  // ];
 
-            <form id = "SetInventoryCheck">
+  //Listen for dropdown click
+  let dropDown = document.getElementById("foodItem");
+  dropDown?.addEventListener("click", addItemsDropDown, { once: true });
 
-              {/*   <label htmlFor="foodItemName">Item Name</label>
+  //Refreshs dropDown children, Puts values from foodItemArr[] to options in dropdown list
+  function addItemsDropDown(event) {
+    //Prevents foodCheckform button from being clickable
+    formButton.disabled = true;
+
+    //Refreshs dropDown's children, removes all options
+    dropDown.replaceChildren();
+
+    event.stopPropagation();
+    event.preventDefault();
+
+    //Takes values from foodItemArr, turn into dropDown options
+    foodItems.forEach((item) => {
+      var option = document.createElement("option");
+      option.text = item.FoodName;
+      dropDown.appendChild(option);
+    });
+  }
+  //-------------
+
+  return (
+    <div className="checkbutton">
+      <button
+        className="invCheck"
+        id="formButton"
+        type="button"
+        onClick={openItem}
+      >
+        Consumed/Spoiled Form
+      </button>
+
+      <h1 className={active ? "cclose" : "copen"}>
+        <form id="SetInventoryCheck">
+          {/*   <label htmlFor="foodItemName">Item Name</label>
                 <input type="text" id="foodItemName"></input> */}
 
-                <label htmlFor="foodItem" >Food Item</label>
-                <br/>
-                <select name="foodItem" id="foodItem">
-                </select>
-                <br/>
-                
-                <label htmlFor="foodItemQuant">Quantity</label>
-                <br/>
-                <input type="number" id="foodItemQuant"></input>
+          <label htmlFor="foodItem">Food Item</label>
+          <br />
+          
+          <select name="foodItem" id="foodItem">
+            {foodItems.map((item, index) => (
+              <option key={index} value={item.FoodName}>
+                {item.FoodName}
+              </option>
+            ))}
+          </select>
+          {/*<select name="foodItem" id="foodItem"></select>*/}
+          <br />
 
-            </form>
-            <br/>
+          <label htmlFor="foodItemQuant">Quantity</label>
+          <br />
+          <input type="number" id="foodItemQuant"></input>
+        </form>
+        <br />
 
-            <button onClick={closeItem}>Consumed</button>
-            <button onClick={closeItem}>Spoiled</button>
-            <button onClick={closeItem}>Cancel</button>
-        </h1>
-
+        <button onClick={closeItem}>Consumed</button>
+        <button onClick={closeItem}>Spoiled</button>
+        <button onClick={closeItem}>Cancel</button>
+      </h1>
     </div>
-)
+  );
 }
