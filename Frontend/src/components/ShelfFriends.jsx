07173d892d7646732d33c59be94f7557019c47d1
Asia Gillispie <agill035@odu.edu>
2025-04-22 20:21:37 -0400

    Added Wrapper around ShelfFriends Page Header

--- Frontend/src/components/ShelfFriends.jsx
+++ Frontend/src/components/ShelfFriends.jsx
@@ -1,12 +1,13 @@
 import Navbar from "./Navbar";
 import FriendList from "./FriendList";
 import Sharing from "./Sharing";
+import './ShelfFriends.css';
 
 const ShelfFriends = () => {
     return  (
         <div>
             <Navbar />
-            <h1>ShelfFriends</h1>
+            <h1 className = "shelfHeader">ShelfFriends</h1>
             <FriendList/>
             <Sharing/>
         </div>
